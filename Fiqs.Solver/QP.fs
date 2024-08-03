4e01d875f0ebf044156aa7eb31a012dbf36dc9ab
Jonathan Schäfer <jonathan.schaefer_@outlook.de>
2024-08-03 14:27:39 +0200

    Refactor solver and tests; remove Newton method
    Removed `Newton.fs` and its references from the project. Added `findMinimum` function in `QP.fs` for simplified Newton step. Updated `objective` function and added `barrierTerm` and `barrierGradient` in `QP.fs`. Modified tests to reflect these changes, including removing `Testing.fsx` and its references.

--- Fiqs.Solver/QP.fs
+++ Fiqs.Solver/QP.fs
@@ -3,6 +3,11 @@ open Types
 open MathNet.Numerics.LinearAlgebra
 open MathNet.Numerics.Optimization
 
+// 1.Newton Schritt für Polynome zweiten Grades immer Minimum
+let findMinimum (Q:Matrix<float>) (c:Vector<float>) (initGuess: Vector<float>) : Vector<float> =
+    initGuess - Q.Inverse() * c
+
+
 let solveQP (p:QPProblem) : (Vector<float> * float) option =
     let Q = p.Q
     let c = p.c
@@ -11,15 +16,26 @@ let solveQP (p:QPProblem) : (Vector<float> * float) option =
     let lB = p.lowerBounds
     let uB = p.upperBounds
 
-    //let objective (x:Vector<float>) : float =
-    //    0.5 * (x.ToRowMatrix() * (Q * x)) + (c * x)
+    let objective (x:Vector<float>) : float =
+        0.5 * ((Q * x) * x) + (c * x)
         
-
     let gradient (x:Vector<float>) : Vector<float> =
         Q * x + c
 
     let constraints (x:Vector<float>) : Vector<float> =
         A * x - b
+
+    let barrierTerm (x: Vector<float>) (lowerBounds: Vector<float>) (upperBounds: Vector<float>) : float =
+        let lowerTerm = x - lowerBounds
+        let upperTerm = upperBounds - x
+        - (lowerTerm.Map(fun xi -> log xi) + upperTerm.Map(fun xi -> log xi)).Sum()
+    
+    let barrierGradient (x: Vector<float>) (lowerBounds: Vector<float>) (upperBounds: Vector<float>) : Vector<float> =
+        let lowerTerm = x - lowerBounds
+        let upperTerm = upperBounds - x
+        lowerTerm.Map(fun xi -> 1.0 / xi) - upperTerm.Map(fun xi -> 1.0 / xi)
+    
+
 
     let bounds =
         [| 
--- Fiqs.Tests/Testing.fsx
+++
@@ -1,6 +0,0 @@
-#r "nuget:MathNet.Numerics.FSharp"
-open MathNet.Numerics.LinearAlgebra
-
-let a = Matrix<float>.Build.DenseOfArray (array2D [ [1;5]; [5; 7]])
-let b = Vector<float>.Build.DenseOfArray [| 3; 2 |]
-let c = a * b
