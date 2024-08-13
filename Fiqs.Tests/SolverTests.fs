d57b82833a855658b37bb6e36ee0c25130e0049c
Jonathan Schäfer <jonathan.schaefer_@outlook.de>
2024-08-13 10:59:26 +0200

    Enhance solveQP with bounds and add new tests
    The `solveQP` function in `QP.fs` was enhanced to include a projection step that ensures the solution vector `new_x` adheres to specified upper and lower bounds. This is done using the `MapIndexed` function to apply the boundaries element-wise.
    
    In `SolverTests.fs`, a new tolerance value `tol` was defined, and a `validate` function was introduced to simplify the process of checking solver results. This function asserts that the solution is optimal and matches the expected results within a specified tolerance.
    
    The test case `Test solveQP for an unconstrained QP problem` was refactored to use the new `validate` function, improving code readability and maintainability.
    
    A new test case `Test solveQP for a simple constrained QP problem` was added to verify the solver's ability to handle quadratic programming problems with constraints. This test sets up a problem with specific bounds and checks if the solver produces the correct solution.

--- Fiqs.Tests/SolverTests.fs
+++ Fiqs.Tests/SolverTests.fs
@@ -6,6 +6,16 @@ open Fiqs
 open MathNet.Numerics.LinearAlgebra
 open Types
 
+let tol = 1e-6
+
+let validate (res:SolverResult) (expSol:Solution) =
+    match res with
+    | Optimal sol ->
+        Assert.True((sol.Result - expSol.Result).L2Norm() < tol && sol.ObjectiveValue = expSol.ObjectiveValue && sol.Iterations <= expSol.Iterations)
+    | Infeasible _ ->
+        Assert.Fail()
+
+
 
 
 [<Fact>]
@@ -37,12 +47,39 @@ let ``Test solveQP for an unconstrained QP problem`` () =
         ObjectiveValue = 0
         Iterations = 1
     }
-    // Solve the QP problem
-    let result = QP.solveQP problem
 
-    // Validate the solution
-    match result with
-    | Optimal sol ->
-        Assert.True (sol.Result = expectedSol.Result && sol.ObjectiveValue = expectedSol.ObjectiveValue && sol.Iterations <= expectedSol.Iterations)
-    | Infeasible _ ->
-        Assert.Fail("Model is infeasible")
+    let result = QP.solveQP problem
+    validate result expectedSol
+
+[<Fact>]
+let ``Test solveQP for a simple constrained QP problem`` () =
+    // f(x) = (x-1)^2 = x^2 -2x + 1 s.t. x < 0
+    let Q = matrix [[2.0]]
+    let c = vector [-2.0]
+    let A = matrix [[0.0]]
+    let y = vector [0.0]
+    let b = vector [0.0]
+    let lowerBounds = vector [Double.NegativeInfinity]
+    let upperBounds = vector [0.0]
+    let tol = 1e-6
+    
+    let problem : QPProblem = {
+        Q = Q
+        c = c
+        A = A
+        y = y
+        b = b
+        lowerBounds = lowerBounds
+        upperBounds = upperBounds
+        constant = 1.0
+        tolerance = tol
+    }
+
+    let expectedSol = {
+        Result = vector [0.0]
+        ObjectiveValue = 1
+        Iterations = 100
+    }
+
+    let result = QP.solveQP problem
+    validate result expectedSol
